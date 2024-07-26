97576682017cb7c76cf6b7847321829b93f96c5d
pieceowater <pieceowater@gmail.com>
2024-07-26 22:38:56 +0500

    bootstrap func

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,33 @@
-export function greet(name: string): string {
-  return `Hello, ${name}!`
+import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+
+export interface PCWTNestClientMicroservicePreset {
+  port: number;
+  microservices: Array<{
+    transport: Transport;
+    urls: string[];
+    queue: string;
+  }>;
+}
+
+export async function bootstrap(appModule: any, config: PCWTNestClientMicroservicePreset) {
+  const app = await NestFactory.create(appModule);
+
+  for (const svc of config.microservices) {
+    app.connectMicroservice<MicroserviceOptions>({
+      transport: svc.transport,
+      options: {
+        urls: svc.urls,
+        queue: svc.queue,
+        queueOptions: {
+          durable: false,
+        },
+      },
+    } as MicroserviceOptions
+    );
+  }
+
+  await app.startAllMicroservices();
+  await app.listen(config.port, '127.0.0.1');
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
